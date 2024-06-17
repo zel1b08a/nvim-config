a3282ce395af82996a3bf679516a796e10079b64
zel1b08a <gh0s-t@yandex.ru>
2024-06-18 03:31:38 +0700

    fix folding

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -164,7 +164,7 @@ vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
 vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
-vim.keymap.set('n', 'F', function()
+vim.keymap.set('n', 'zF', function()
     local winid = require('ufo').peekFoldedLinesUnderCursor()
     if not winid then
         -- choose one of coc.nvim and nvim lsp
